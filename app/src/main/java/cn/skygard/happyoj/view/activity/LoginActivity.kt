9a4dfe6e5a5a05f5b3543756a61832671f64e937
iGxnon <1545766400@qq.com>
2022-09-22 13:14:36 +0800

    fix Jenkinsfile

--- app/src/main/java/cn/skygard/happyoj/view/activity/LoginActivity.kt
+++ app/src/main/java/cn/skygard/happyoj/view/activity/LoginActivity.kt
@@ -14,6 +14,7 @@ import cn.skygard.common.base.ui.BaseBindActivity
 import cn.skygard.happyoj.R
 import cn.skygard.happyoj.databinding.ActivityLoginBinding
 import cn.skygard.happyoj.repo.remote.RetrofitHelper
+import cn.skygard.happyoj.view.fragment.LoginFragment
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
 import java.util.regex.Pattern
@@ -42,8 +43,11 @@ class LoginActivity : BaseBindActivity<ActivityLoginBinding>() {
             setDisplayHomeAsUpEnabled(true)
             title = ""
         }
-        binding.btnLogin.setOnClickListener(LoginCallback())
-        binding.btnRegister.setOnClickListener(RegisterCallback())
+        replaceFragment(R.id.frag_container) {
+            LoginFragment.newInstance()
+        }
+//        binding.btnLogin.setOnClickListener(LoginCallback())
+//        binding.btnRegister.setOnClickListener(RegisterCallback())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -58,34 +62,5 @@ class LoginActivity : BaseBindActivity<ActivityLoginBinding>() {
             ctx.startActivity(Intent(ctx, LoginActivity::class.java))
         }
     }
-
-    private inner class LoginCallback : View.OnClickListener {
-        override fun onClick(v: View?) {
-        }
-    }
-
-    private inner class RegisterCallback : View.OnClickListener {
-        val pattern = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$"
-        override fun onClick(v: View?) {
-            val email = binding.etUsername.text
-            val pwd = binding.etPwd.text
-            if (pwd.length > 30) {
-                Toast.makeText(this@LoginActivity, "密码太长", Toast.LENGTH_SHORT).show()
-                return
-            }
-            if (pwd.length < 6) {
-                Toast.makeText(this@LoginActivity, "密码太短", Toast.LENGTH_SHORT).show()
-                return
-            }
-            if (!Pattern.compile(pattern).matcher(email).matches()) {
-                Toast.makeText(this@LoginActivity, "请输入邮箱注册", Toast.LENGTH_SHORT).show()
-                return
-            }
-            lifecycleScope.launch {
-                RetrofitHelper.userService.register("未命名", pwd.toString(), email.toString())
-            }
-        }
-    }
-
 
 }
